b4a90a83b89f3d4e0f1d46154dbd0538a6e4fe2b
Lucas Passeado <lpasseado@gmail.com>
2021-09-12 18:55:55 -0300

    commit

--- index.js
+++ index.js
@@ -129,9 +129,9 @@ const main = async () => {
         return response.redirect("/login")
     })
 
-    app.listen(5050, () => {
-        console.log('server running at http://localhost:5050')
-    })
+    app.listen(process.env.PORT || 5000, () => {
+        console.log("Rodando ;) | PORT=5000")
+    }) 
 }
 
 main()
